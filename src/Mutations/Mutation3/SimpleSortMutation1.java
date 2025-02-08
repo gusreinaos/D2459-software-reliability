b75b1a802ba69a0d8ad6a76c129d6974f999fac2
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2025-02-08 15:48:31 +0100

    feat: modify mutation 3 as it was repeated

--- src/Mutations/Mutation3/SimpleSortMutation1.java
+++ src/Mutations/Mutation3/SimpleSortMutation1.java
@@ -3,13 +3,14 @@ package Mutations.Mutation3;
 public class SimpleSortMutation1 {
     public static void sort(int[] A) throws Exception{
         for (int i = 0; i < A.length - 1; i++) {
-            for (int j = 0; j < A.length - 1 - i; j++) {
-                if (A[j] < A[j + 1]) { // CHANGED: descending orer
+            for (int j = 0; j < A.length - 1 - i ; j++) {
+                if (A[j] > A[j + 1]) {
                     int temp = A[j];
                     A[j] = A[j + 1];
                     A[j + 1] = temp;
                 }
             }
+            if (i > 0) A[i] = A[i-1]; //CHANGED: Added lines that values are being copied and propagated
         }
     }
 }
